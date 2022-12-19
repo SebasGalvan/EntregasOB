6b6f0c0b854b2840818688d68c605474142e0578
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-12-18 21:53:14 -0300

    Entrega modulo 3

--- src/src/main/java/org/modulos/modulo3.java
+++ src/src/main/java/org/modulos/modulo3.java
@@ -3,9 +3,12 @@ package org.modulos;
 public class modulo3 {
 
     public double getPrecio(double precio, double iva){
-        iva = precio * iva;
+        iva = (precio * iva)/100;
         return precio + iva;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new modulo3().getPrecio(50,10));
+    }
 
 }
